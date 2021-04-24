8d498eac2fbda000a09d2d09ce77b426b89a8907
Roberto Mitsuyasu <mitras25jpn@hotmail.com>
2021-04-24 15:58:24 -0300

    Niteo 1

---
+++ niteo1/js/main.js
@@ -0,0 +1,13 @@
+$('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+$('.carousel2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
